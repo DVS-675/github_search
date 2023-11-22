4be5f4580d7ae986cd460a8c3c47b2e95437e621
Dmitry <Dmitryvs20170@gmail.com>
2023-11-22 18:26:53 +0500

    add sort

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import "./App.css";
-import { getUser } from "./api";
+import {getUser, getUserRepos} from "./api";
 
 function App() {
   const [login, setLogin] = useState("");
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState(null);
   const [page, setPage] = useState(1);
+  const [sort, setSort] = useState(false)
 
   const incrementPage = () => {
     setPage((prev) => prev + 1);
-    handleSearch();
   };
 
   const handleSearch = async () => {
     const responseData = await getUser(login, page);
-    setUsers(responseData);
+    setUsers(responseData.items.map((user) => {
+      return {...user, repositories:[]}
+    }));
   };
+
+
+  const handleSort = () => {
+    setSort(!sort)
+    if (sort) {
+      let sortUsers = users;
+      sortUsers.sort(function (a, b) {
+        return a.repositories.length - b.repositories.length
+      })
+      setUsers(sortUsers)
+    } else if (!sort) {
+      let sortUsers = users;
+      sortUsers.sort(function (a, b) {
+        return b.repositories.length - a.repositories.length
+      })
+      setUsers(sortUsers)
+    }
+  }
+
+  useEffect(() => {
+    if (users ) {
+      let requests = users.map(user => getUserRepos(user.login));
+      Promise.all(requests)
+          .then(responses => responses.forEach(response => {
+            response.map((repo) => {
+              users.map((user) => {
+                if (user.login === repo.owner.login) {
+                   user.repositories.push(repo)
+                }
+              })
+            })
+          }));
+    }
+  }, [users]);
+
+  useEffect(() => {
+    if (users) {
+      getUser(login, page)
+          .then((res) => {
+            setUsers([...users, ...res.items.map((user) => {
+              return {...user, repositories:[]}
+            })])
+          })
+
+    }
+  }, [page]);
+
+  console.log(users)
 
   return (
     <div className="App">
@@ -32,19 +82,34 @@ function App() {
             Search
           </button>
         </div>
+        <div role='none' onClick={handleSort} className='search_sort'>
+          <p>sort by number of repositories</p>
+          <img
+              className={sort ? ("search_sort_select") : ("search_sort_select_rev")}
+              src='/img/arrow.svg'
+              alt="select"
+          />
+        </div>
         <div className="box">
-          {users &&
-            users.items.length &&
-            users.items.map((item) => (
-              <div className="box__user">
-                <img
-                  className="box__user_image"
-                  src={item.avatar_url}
-                  alt="avatar"
-                />
-                <div className="box__user_login">{item.login}</div>
-              </div>
-            ))}
+          {(users &&
+            users.length && users[0] && users[0].repositories) ? (
+              users.map((item) => (
+                      <div className="box__user">
+                        <img
+                            className="box__user_image"
+                            src={item.avatar_url}
+                            alt="avatar"
+                        />
+                          <div className='box__title'>{item.repositories.length}</div>
+
+
+                        <div className="box__user_login">{item.login}</div>
+                      </div>
+                  )
+              )
+            ) : (
+                  <div className='box__title'>users not found</div>
+          )}
         </div>
         <button onClick={incrementPage} className="button">
           Load more
--- src/api.js
+++ src/api.js
@@ -1,17 +1,50 @@
 const userPerPage = 5;
+const token =
+    "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";
+
 
 export function getUser(login, page) {
-  console.log(page);
   return fetch(
     `https://api.github.com/search/users?q=${login}&per_page=${userPerPage}&page=${page}`,
     {
       method: "GET",
-      headers: {
-        "content-type": "application/json",
-      },
+        headers: {
+            Authorization: token,
+        },
     }
   ).then((response) => {
-    console.log(response);
     return response.json();
   });
 }
+
+export function getUserRepos(login) {
+    return fetch(
+        `https://api.github.com/users/${login}/repos?&per_page=100`,
+        {
+            method: "GET",
+            headers: {
+               Authorization: token,
+            },
+        }
+    ).then((response) => {
+        return response.json();
+    });
+}
+
+
+//  для информации о пользователе ${BASE_PATH}users/${userName}`
+// export function getUserNew(login) {
+//     return fetch(
+//         `${API_PATH}?q=` +
+//         encodeURIComponent(`${login} in:login sort:repositories`) +
+//         "&per_page=100",
+//         {
+//             headers: {
+//                 Authorization: token,
+//             },
+//         }
+//     ).then((response) => {
+//         console.log(response);
+//         return response.json();
+//     });
+// }
